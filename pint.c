845234e018e704aaece5914f9ec060c42ff09810
Drixner <drixnerrafael1@gmail.com>
2022-08-17 20:40:04 -0500

    creating the function pint

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+
+/**
+ *pint - prints the value at the top of the stack
+ *@stack: the stack to print the top element of
+ *@line_number: the line number where the particular
+ *              opcode function is specified
+ * 
+ *Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		pint_error(line_number);
+	printf("%d\n", (*stack)->n);
+}
