d128f3019307ebd1cc18db71c80888be59534c91
Drixner <drixnerrafael1@gmail.com>
2022-08-17 21:42:43 -0500

    add new line pop

--- error.c
+++ error.c
@@ -55,3 +55,16 @@ void pint_error(unsigned int line_number)
 	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ *pop_error - prints an error message then exits
+ *@line_number: the line number in the byte code file where
+ *              the pop opcode is specified
+ *Description: prints an error message when push there is no
+ *             argument given to push or when a non-integer is given
+ *Return: void
+ */
+void pop_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
