7c1ea68c9159dd2c26d6e6e879128ec031f308c6
Drixner <drixnerrafael1@gmail.com>
2022-08-17 22:47:38 -0500

    add prototype spwap

--- monty.h
+++ monty.h
@@ -18,6 +18,7 @@
 		{"pall", pall},			\
 		{"pint", pint},			\
 		{"pop", pop},			\
+		{"swap", swap},			\
 		{NULL, NULL}			\
 	}
 
@@ -77,5 +78,6 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif
